fb1c3bd7d275a0101750e16e96439dd1dfa2b7ee
raymond dzery hago <raymond.rakotonirainy@bubble-go.mg>
2024-08-13 12:54:31 +0300

    chore: configuration and init

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    PrismaModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
